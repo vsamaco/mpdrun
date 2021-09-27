b866ae45c2720a50eebd476d4fb1d6092580d942
Vincent Samaco <vsamaco@gmail.com>
2021-09-26 18:09:34 -0700

    Move page tests outside to avoid build conflict

--- src/__tests__/pages/index.test.js
+++ src/__tests__/pages/index.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import renderer from 'react-test-renderer'
-import Home from '../index'
+import Home from '../../pages/index'
 
 describe('Home', () => {
   const athlete = {
