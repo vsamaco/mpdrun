28feed021ffdab761928a96132ddb1ee5a167179
Vincent Samaco <vsamaco@gmail.com>
2023-04-24 01:00:47 -0700

    update tests and shoe merge

--- src/components/__tests__/Run.test.js
+++ src/components/__tests__/Run.test.js
@@ -4,25 +4,25 @@ import '@testing-library/jest-dom/extend-expect'
 import Run from '../Run'
 
 describe('Run', () => {
-  const activity = {
+  const activities = [{
     name: 'Activity Name',
     distance: 1000,
     moving_time: 360,
     total_elevation_gain: 0 
-  };
+  }];
 
   it('renders title', () => {
-    render(<Run activity={activity} />)
+    render(<Run activities={activities} />)
     expect(screen.getByRole('heading', {level: 3})).toHaveTextContent(/Run/i);
   });
 
   it('renders activity name', () => {
-    render(<Run activity={activity} />)
+    render(<Run activities={activities} />)
     expect(screen.getByRole('heading', {level: 4})).toHaveTextContent(/Activity Name/i);
   })
 
   it('render stats', () => {
-    render(<Run activity={activity} />)
+    render(<Run activities={activities} />)
     const stats = screen.getAllByRole('listitem');
     expect(stats[0]).toHaveTextContent('0.62 mi')
     expect(stats[1]).toHaveTextContent('0h 6m')
